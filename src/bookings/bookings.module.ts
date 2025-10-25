7cd29707f08db0e49f0902317adddff9e6670e8b
Faizan Khan <mohd.faizan1309@gmail.com>
2025-10-26 02:54:43 +0530

    chore(api): Register AvailabilityService in BookingsModule

--- src/bookings/bookings.module.ts
+++ src/bookings/bookings.module.ts
@@ -7,11 +7,12 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { RedisModule } from '../redis/redis.module';
 import { ErrorHandlerService } from '../common/services/error-handler.service';
 import { VenueBookingsController } from './controllers/venue-bookings.controller';
+import { AvailabilityService } from './services/availability.service';
 
 @Module({
   imports: [PrismaModule, RedisModule, UsersModule],
   controllers: [BookingsController, VenueBookingsController],
-  providers: [BookingsService, BookingNumberService, ErrorHandlerService],
-  exports: [BookingsService, BookingNumberService],
+  providers: [BookingsService, BookingNumberService, ErrorHandlerService, AvailabilityService],
+  exports: [BookingsService, BookingNumberService, AvailabilityService],
 })
 export class BookingsModule {}
