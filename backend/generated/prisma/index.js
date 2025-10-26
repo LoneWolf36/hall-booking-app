93dd543f418c22f4dd172ee69ca618971859b710
Faizan Khan <runefaizan36@gmail.com>
2025-10-27 02:33:43 +0530

    UI fixes

--- backend/generated/prisma/index.js
+++ backend/generated/prisma/index.js
@@ -322,7 +322,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
