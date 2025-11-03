df357bfef1c3244f8dc09236549686d560be4865
Faizan Khan <mohd.faizan1309@gmail.com>
2025-11-03 19:36:48 +0530

    fix(venues): register VenueTimeslotsController in VenuesModule to enable /venues/:id/timeslots routes

--- backend/src/venues/venues.module.ts
+++ backend/src/venues/venues.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { VenuesController } from './venues.controller';
 import { VenuesService } from './venues.service';
+import { VenueTimeslotsController } from './venue-timeslots.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [VenuesController],
+  controllers: [VenuesController, VenueTimeslotsController],
   providers: [VenuesService],
   exports: [VenuesService],
 })
